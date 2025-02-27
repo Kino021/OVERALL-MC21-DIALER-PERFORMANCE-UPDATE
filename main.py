6642b0e9add3676ce821b4d3741786966fd2eaae
Kino021 <kpilustrisimo@spmadridlaw.com>
2025-02-27 17:26:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -178,32 +178,3 @@ if uploaded_file is not None:
             }])], ignore_index=True)
         
         st.write(collector_summary)
-    with col6:
-        st.write("## Claim Paid Summary Table")
-
-        # Add date filter
-        min_date = df['Date'].min().date()
-        max_date = df['Date'].max().date()
-        start_date, end_date = st.date_input("Select date ranges", [min_date, max_date], min_value=min_date, max_value=max_date)
-
-        filtered_df = df[(df['Date'].dt.date >= start_date) & (df['Date'].dt.date <= end_date)]
-
-        cp_collector_summary = pd.DataFrame(columns=[
-            'Day', 'Collector', 'Total Claim Paid','Claim Paid Amount','Balance Amount'
-        ])
-        
-        for (date, collector), collector_group in filtered_df[~filtered_df['Remark By'].str.upper().isin(['SYSTEM'])].groupby([filtered_df['Date'].dt.date, 'Remark By']):
-            claim_paid_count = collector_group[collector_group['Reason For Default'].str.contains('CURED', na=False) ]['Account No.'].nunique()
-            claim_paid_amount = collector_group[collector_group['Reason For Default'].str.contains('CURED', na=False)]['Claim Paid Amount'].sum()
-            balance_amount = collector_group[collector_group['Reason For Default'].str.contains('CURED', na=False) & (collector_group['Balance'] != 0)]['Balance'].sum()
-            
-            
-            cp_collector_summary = pd.concat([cp_collector_summary, pd.DataFrame([{
-                'Day': date,
-                'Collector': collector,
-                'Total Claim Paid': claim_paid_count,
-                'Claim Paid Amount': claim_paid_amount,
-                'Balance Amount': balance_amount
-            }])], ignore_index=True)
-        
-        st.write(cp_collector_summary)
