8f09d7269f16b9dfc089e9e4ad2fa0c1c9d10737
Kino021 <kpilustrisimo@spmadridlaw.com>
2025-03-14 17:52:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -176,11 +176,12 @@ if uploaded_file is not None:
             overall_manual_table = calculate_summary(df, 'Outgoing')
             st.write(overall_manual_table)
 
-        # Summary Table by Cycle Predictive
+        # Summary Table by Cycle Predictive (Modified)
         st.write("## Summary Table by Cycle Predictive")
         for cycle, cycle_group in df.groupby('Service No.'):
             st.write(f"Cycle: {cycle}")
-            summary_table = calculate_summary(cycle_group, 'Predictive', 'SYSTEM')
+            cycle_group_filtered = cycle_group[cycle_group['Remark Type'].isin(['Follow Up', 'Predictive'])]
+            summary_table = calculate_summary(cycle_group_filtered, 'Predictive', 'SYSTEM')
             st.write(summary_table)
 
         # Summary Table by Cycle Manual
